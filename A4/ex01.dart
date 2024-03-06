734c0b210448981d73879e9569dae83458ddbdb1
D A N Z     gLdツ <133895324+SoarezGOLdENbOY@users.noreply.github.com>
2024-03-06 08:01:25 -0300

    Add files via upload

---
+++ A4/ex01.dart
@@ -0,0 +1,15 @@
+void main() {
+  // Agregando valores ao map
+  Map<String, double> frutas = {
+    "Maçã": 1.5,
+    "Banana": 0.8,
+    "Laranja": 1.2,
+    "Uva": 2.0,
+    "Abacaxi": 2.5,
+  };
+  print("Preço das frutas:");
+  // Estrutura de repetição
+  for (var valores in frutas.entries) {
+    print("${valores.key}: R\$${valores.value}");
+  }
+}

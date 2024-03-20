31542075018f01b0335a117ccd76d8739a926f5e
D A N Z     gLdツ <133895324+SoarezGOLdENbOY@users.noreply.github.com>
2024-03-20 15:27:48 -0300

    Add files via upload

---
+++ A7/a7classes.dart
@@ -0,0 +1,8 @@
+void main() {
+    Carro carro = Carro("ABC1234", 123456);
+ 
+    Animal animal = Animal(1.5, "Marrom");
+    
+    Consulta consulta = Consulta(DateTime.now(), "João", "Dr. Silva");
+
+}
